d2e352f71e85c2247317626b7b63ca888f63449f
Tanay Toshniwal <tanaytoshniwal902@gmail.com>
2024-08-20 15:16:32 +0530

    Add room and roomcategory entity

--- Entities/Class1.cs
+++ Entities/Class1.cs
@@ -37,7 +37,7 @@ namespace Entities
         }
 
         private string owner;
-        public string Owner;
+        public string Owner
         {
             get { return owner; }
             set { owner = value; }
@@ -47,17 +47,74 @@ namespace Entities
     public class HotelCategory
     {
         private int id;
-        public int Id;
+        public int Id
         {
             get { return id; }
             set { Id = value; }
         }
 
         private string name;
-        public string Name;
+        public string Name
         {
             get { return name; }
             set { name = value; }
         }
+    }
+}
+
+public class Room
+{
+    private int id;
+    public int ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    private int categoryID;
+    public int CategoryID
+    {
+        get { return categoryID; }
+        set { categoryID = value; }
+    }
+
+    private int rateID;
+
+    public int RateID
+    {
+        get { return rateID; }
+        set { rateID = value; }
+    }
+
+
+    public Room()
+    {
+        id = 0;
+        categoryID = 0;
+        rateID = 0;
+    }
+
+}
+
+public class RoomCategory
+{
+    private int id;
+    public int ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    RoomCategory()
+    {
+        id = 0;
+        name = string.Empty;
     }
 }
