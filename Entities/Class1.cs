c8d7fe2d95cc21e18d9656b661e4abc53106778f
Shreya Gupta <shreyag0104@gmail.com>
2024-08-20 15:09:20 +0530

    Added hotel,hotelCategory entities

--- Entities/Class1.cs
+++ Entities/Class1.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
-    public class Class1
+    public class Hotel
     {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private int categoryId;  // Foreign key to HotelCategory
+        public int CategoryId
+        {
+            get { return categoryId; }
+            set { categoryId = value; }
+        }
+
+        private string location;
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        private string contactDetails;
+        public string ContactDetails
+        {
+            get { return contactDetails; }
+            set { contactDetails = value; }
+        }
+
+        private string owner;
+        public string Owner;
+        {
+            get { return owner; }
+            set { owner = value; }
+        }
+    }
+
+    public class HotelCategory
+    {
+        private int id;
+        public int Id;
+        {
+            get { return id; }
+            set { Id = value; }
+        }
+
+        private string name;
+        public string Name;
+        {
+            get { return name; }
+            set { name = value; }
+        }
     }
 }
